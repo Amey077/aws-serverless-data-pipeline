43b4edae2c0e43d2f25d77b293edec47d1f48334
unknown <iameymahajan5@gmail.com>
2026-03-16 10:19:30 +0530

    add retrieve lambda code

--- lambda/retrieve/retreieve.py
+++ lambda/retrieve/retreieve.py
@@ -1,3 +1,30 @@
+import requests
+import boto3
+import os
+
+s3 = boto3.client("s3")
+
+DATA_URL = "https://eforexcel.com/wp/wp-content/uploads/2020/09/2m-Sales-Records.zip"
+
+BUCKET = "aws-serverless-data-pipeline-s3-bucket"
+KEY = "raw_data/sales_records.zip"
+
+
 def handler(event, context):
-    print("Lambda placeholder")
-    return {"status": "ok"}
+
+    print(f"Downloading dataset from {DATA_URL}")
+
+    response = requests.get(DATA_URL)
+
+    if response.status_code != 200:
+        raise Exception("Failed to download dataset")
+
+    print("Uploading to S3...")
+
+    s3.put_object(
+        Bucket=BUCKET,
+        Key=KEY,
+        Body=response.content
+    )
+
+    return {"status": "success"}
